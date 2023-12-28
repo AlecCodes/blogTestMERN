1dac248f1989504d922c4af096ee970d391e58d4
Alec Hannaford <alec322@gmail.com>
2023-12-27 17:05:58 -0800

    login route

--- routes/users.js
+++ routes/users.js
@@ -8,16 +8,36 @@ router.get("/", (req,res) => {
 })
 
 router.post("/signup" , async (req,res) => {
-    req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
+  req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
 
-    try {
-        const user = await User.create(req.body)
-        res.status(201).json({message: "USER CREATED "+ user})
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({error:"Bad errorrrrr"})
-    }
+  try {
+      const user = await User.create(req.body)
+      res.status(201).json({message: "USER CREATED "+ user})
+  } catch (error) {
+      console.error(error)
+      res.status(500).json({error:"Bad errorrrrr"})
+  }
 
+})
+
+router.post("/login", async (req,res) => {
+  const {username , password} = req.body
+  const user = User.findOne({username})
+  if (!user){
+    res.send("USER DOES NOT EXIST")
+  } else {
+    const result = bcrypt.compareSync(password, user.password)
+    if (result){
+      req.session.username = username
+      //verify loggedIn in authentication middleware to protect admin route?
+      req.session.loggedIn = true
+      console.log("ARE YOU LOGGED IN? " + req.session.loggedIn)
+      res.send("Logged in! Welcome " + user.username)
+      //PUT REDIRECTS HERE!
+    } else {
+      res.send("WRONG PASSWORD")
+    } 
+  }
 })
 
 module.exports = router
