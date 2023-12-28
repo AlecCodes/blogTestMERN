690454837906f2392be4592b0f4a4c5755ad6319
Alec Hannaford <alec322@gmail.com>
2023-12-27 16:51:45 -0800

    signup route

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,17 @@ router.get("/", (req,res) => {
     res.send("UESRRR RIOUTTEE")
 })
 
+router.post("/signup" , async (req,res) => {
+    req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10))
+
+    try {
+        const user = await User.create(req.body)
+        res.status(201).json({message: "USER CREATED "+ user})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error:"Bad errorrrrr"})
+    }
+
+})
+
 module.exports = router
